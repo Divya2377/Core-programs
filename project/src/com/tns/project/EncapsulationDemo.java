72262d6888b4954e8ff4227899f2bb322d016c74
acer <acer@10.0.0.109>
2023-09-22 14:38:01 +0530

    second commit

---
+++ project/src/com/tns/project/EncapsulationDemo.java
@@ -0,0 +1,15 @@
+package com.tns.project;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encapsulation ob=new Encapsulation();//creating object
+		ob.serialno=10;//accessing the data member by using dot operator
+		ob.name="Divya";
+		ob.age=21;
+		ob.show();
+
+	}
+
+}
