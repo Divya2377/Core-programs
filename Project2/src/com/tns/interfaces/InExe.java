ce09c4352961cde76461f5acf8b694967a803839
acer <acer@10.0.0.100>
2023-10-04 12:22:36 +0530

    First commit

---
+++ Project2/src/com/tns/interfaces/InExe.java
@@ -0,0 +1,13 @@
+package com.tns.interfaces;
+
+public class InExe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		InDemo i=new InDemo();
+		i.show();
+		i.print();
+
+	}
+
+}
